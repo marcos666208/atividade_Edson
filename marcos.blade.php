61879176d40c897490b910ac8756625334f70b68
marcos666208 <107429858+marcos666208@users.noreply.github.com>
2023-11-08 07:46:07 -0300

    Add files via upload

---
+++ marcos.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Svelte</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Marcos Alexandre</h1>
+        <p>Prefiro Svelte.</p>
+    </div>
+</body>
+</html>
